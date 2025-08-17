5a87aff711a9f6196f983b4c25e2dd98b562289f
cinap_lenrek <cinap_lenrek@felloff.net>
2025-08-17 19:52:18 +0000

    libc: fix aswap*() for arm
    remove useless MOVL $0, 5 in agetl().
    use LDREX/STREX for *ATOMIC* aswap*().
    no refunds.

--- sys/src/libc/arm/atomic.s
+++ sys/src/libc/arm/atomic.s
@@ -3,7 +3,6 @@
 /* get variants */
 TEXT agetl+0(SB),1,$0
 TEXT agetp+0(SB),1,$0
-	MOVW	$0, R5
 	MOVW	(R0), R0
 	DMB	$ISH
 	RET
@@ -12,23 +11,25 @@ TEXT agetp+0(SB),1,$0
 TEXT aswapl+0(SB),1,$0
 TEXT aswapp+0(SB),1,$0
 	MOVW	new+4(FP), R1
-	MOVW	(R0), R2
-	DMB	$ISH
-	MOVW	R1, (R0)
-	MOVW	R2, R0
+	MOVW	R0, R2
+_aswapl:
+	LDREX	(R2), R0
+	STREX	R1, (R2), R3
+	CMP	$0, R3
+	BNE	_aswapl
 	DMB	$ISH
 	RET
 
 /* inc variants */
 TEXT aincl+0(SB),1,$0
 	MOVW	delta+4(FP), R1
+	MOVW	R0, R2
 _aincl:
-	LDREX	(R0), R3
-	ADD	R1, R3
-	STREX	R3, (R0), R4
-	CMP	$0, R4
+	LDREX	(R2), R0
+	ADD	R1, R0
+	STREX	R0, (R2), R3
+	CMP	$0, R3
 	BNE	_aincl
-	MOVW	R3, R0
 	DMB	$ISH
 	RET
 
@@ -42,8 +43,8 @@ _acasl:
 	LDREX	(R0), R3
 	CMP	R1, R3
 	BNE	_acaslf
-	STREX	R2, (R0), R4
-	CMP	$0, R4
+	STREX	R2, (R0), R3
+	CMP	$0, R3
 	BNE	_acasl
 	MOVW	$1, R0
 	DMB	$ISH
